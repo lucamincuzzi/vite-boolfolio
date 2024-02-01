2e8b6592c7a4c01c51a1375a9342c130d20b0886
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-02-01 19:42:02 +0100

    add: scaffolding

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import AppMain from "./components/AppMain.vue";
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {AppMain}
+}
+</script>
+
+<template>
+<h1>Hello world!</h1>
+<AppMain />
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,9 @@
+<script></script>
+<template>
+    <h2>My first component</h2>
+</template>
+<style lang="scss" scoped>
+h2 {
+    color: purple;
+}
+</style>
